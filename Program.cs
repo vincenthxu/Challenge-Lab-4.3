b8b88948404df1d8fc4ab0aa00d36f7bb0e13cb5
Vincent Xu <vincent.xu1@outlook.com>
2025-07-29 14:11:03 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Challenge_Lab_4._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                char[] s = ['h', 'e', 'l', 'l', 'o'];
+                StringReverse(ref s);
+                Console.WriteLine(s);
+            }
+            else
+            {
+                foreach(var arg in args)
+                {
+                    char[] chars = arg.ToCharArray();
+                    StringReverse(ref chars);
+                    Console.WriteLine(chars);
+                }
+            }
+        }
+
+        static void StringReverse(ref char[] characters)
+        {
+            char temp;
+            for (int i = 0; i < characters.Length / 2; i++)
+            {
+                temp = characters[i];
+                characters[i] = characters[characters.Length - i - 1];
+                characters[characters.Length - i - 1] = temp;
+            }
+        }
+    }
+}
